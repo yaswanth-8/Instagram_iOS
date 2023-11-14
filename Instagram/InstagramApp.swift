7b80ce7079c140194bbf5cf3d3a55d6d6f53c5b3
yaswanth <yaswanthm@presidio.com>
2023-11-14 10:41:32 +0530

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Yaswanth on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
